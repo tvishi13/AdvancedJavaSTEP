cb27262d560f0d70052a35e2717e23d851c89eda
tv <tvishidalmiya1327@gmail.com>
2026-03-25 14:55:18 +0530

    Implement AccountIDLookup with search methods

---
+++ Week3-4/AccountIDLookup.java
@@ -0,0 +1,32 @@
+public class AccountIDLookup {
+
+    static int linearSearch(String[] arr, String target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(target)) return i;
+        }
+        return -1;
+    }
+
+    static int binarySearch(String[] arr, String target) {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid].equals(target)) return mid;
+            else if (arr[mid].compareTo(target) < 0)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+
+    static int countOccurrences(String[] arr, String target) {
+        int count = 0;
+        for (String s : arr) {
+            if (s.equals(target)) count++;
+        }
+        return count;
+    }
+}
