6a49118826c2d7909a023c428456a4c36d1a4249
tv <tvishidalmiya1327@gmail.com>
2026-03-25 14:58:03 +0530

    Create TransactionFee.java

---
+++ Week3-4/TransactionFee.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Transaction {
+    String id;
+    double fee;
+    String timestamp;
+
+    Transaction(String id, double fee, String timestamp) {
+        this.id = id;
+        this.fee = fee;
+        this.timestamp = timestamp;
+    }
+}
+
+public class TransactionFee {
+
+    // Bubble Sort (by fee)
+    static void bubbleSort(List<Transaction> list) {
+        int n = list.size();
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list.get(j).fee > list.get(j + 1).fee) {
+                    Collections.swap(list, j, j + 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+    // Insertion Sort (fee + timestamp)
+    static void insertionSort(List<Transaction> list) {
+        for (int i = 1; i < list.size(); i++) {
+            Transaction key = list.get(i);
+            int j = i - 1;
+
+            while (j >= 0 &&
+                    (list.get(j).fee > key.fee ||
+                    (list.get(j).fee == key.fee &&
+                     list.get(j).timestamp.compareTo(key.timestamp) > 0))) {
+                list.set(j + 1, list.get(j));
+                j--;
+            }
+            list.set(j + 1, key);
+        }
+    }
+
+    static void findOutliers(List<Transaction> list) {
+        for (Transaction t : list) {
+            if (t.fee > 50) {
+                System.out.println("Outlier: " + t.id);
+            }
+        }
+    }
+}
