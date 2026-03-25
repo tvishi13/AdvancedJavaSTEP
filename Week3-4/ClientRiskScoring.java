b4084fb9f252321d1c61cb3022da2db54d2e7820
tv <tvishidalmiya1327@gmail.com>
2026-03-25 14:56:29 +0530

    Create ClientRiskScoring.java

---
+++ Week3-4/ClientRiskScoring.java
@@ -0,0 +1,48 @@
+class Client {
+    String name;
+    int riskScore;
+    double balance;
+
+    Client(String name, int riskScore, double balance) {
+        this.name = name;
+        this.riskScore = riskScore;
+        this.balance = balance;
+    }
+}
+
+public class ClientRiskScoring {
+
+    static void bubbleSort(Client[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j].riskScore > arr[j + 1].riskScore) {
+                    Client temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void insertionSort(Client[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            Client key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 &&
+                  (arr[j].riskScore < key.riskScore ||
+                  (arr[j].riskScore == key.riskScore &&
+                   arr[j].balance < key.balance))) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    static void topClients(Client[] arr, int k) {
+        for (int i = 0; i < k; i++) {
+            System.out.println(arr[i].name + " " + arr[i].riskScore);
+        }
+    }
+}
