d23c373161005f21e414f2d5b45d07204bc83127
tv <tvishidalmiya1327@gmail.com>
2026-03-25 14:57:02 +0530

    Implement sorting algorithms for asset portfolio

---
+++ Week3-4/PortfolioReturnSorting.java
@@ -0,0 +1,70 @@
+class Asset {
+    String name;
+    double returnRate;
+    double volatility;
+
+    Asset(String name, double r, double v) {
+        this.name = name;
+        this.returnRate = r;
+        this.volatility = v;
+    }
+}
+
+public class PortfolioReturnSorting {
+
+    static void mergeSort(Asset[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    static void merge(Asset[] arr, int l, int m, int r) {
+        Asset[] temp = new Asset[r - l + 1];
+        int i = l, j = m + 1, k = 0;
+
+        while (i <= m && j <= r) {
+            if (arr[i].returnRate <= arr[j].returnRate)
+                temp[k++] = arr[i++];
+            else
+                temp[k++] = arr[j++];
+        }
+
+        while (i <= m) temp[k++] = arr[i++];
+        while (j <= r) temp[k++] = arr[j++];
+
+        System.arraycopy(temp, 0, arr, l, temp.length);
+    }
+
+    static void quickSort(Asset[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    static int partition(Asset[] arr, int low, int high) {
+        Asset pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j].returnRate > pivot.returnRate ||
+               (arr[j].returnRate == pivot.returnRate &&
+                arr[j].volatility < pivot.volatility)) {
+                i++;
+                Asset temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        Asset temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+}Week3-4/PortfolioReturnSorting.java
